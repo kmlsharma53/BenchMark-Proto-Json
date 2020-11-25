104713374d9fbebb4f9bea3cbc2ce45c36a9eedf
kmlsharma53 <kmlsharma53@gmail.com>
2020-11-25 20:03:40 +0530

    Initial commit

--- src/main/java/BenchMark.java
+++ src/main/java/BenchMark.java
@@ -2,19 +2,43 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.protobuf.InvalidProtocolBufferException;
 import proto.Employee;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class BenchMark {
+
+    static FileWriter fileWriter = null;
+
+    static {
+        try {
+            fileWriter = new FileWriter("/Users/kamalsharma/Desktop/resp_1000000.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args){
 
-        json.Employee empJ = new json.Employee();
-        empJ.setName("Kamal Sharma");
 
-        empJ.setEmpId("PAYTM-26847");
-        empJ.setAddress("Pawan Vihar Colony , Pithoragarh, Uttarakhand");
-        empJ.setEmail("kamal.sharma@paytm.com");
-        empJ.setPhoneNo(9911729459L);
 
         ObjectMapper mapper = new ObjectMapper();
+
+
+
+        json.Employee empJ = new json.Employee();
+                empJ.setName("Kamal Sharma");
+
+                empJ.setEmpId("PAYTM-26847");
+                empJ.setAddress("Pawan Vihar Colony , Pithoragarh, Uttarakhand");
+                empJ.setEmail("kamal.sharma@paytm.com");
+                empJ.setPhoneNo(9911729459L);
+
+        proto.Employee empP = Employee.newBuilder().
+                setName("Kamal Sharma")
+                .setEmpId("PAYTM-26847")
+                .setAddress("Pawan Vihar Colony , Pithoragarh, Uttarakhand")
+                .setEmail("kamal.sharma@paytm.com")
+                .setPhoneNo(9911729459L).build();
 
         Runnable json = () -> {
 
@@ -26,13 +50,6 @@ public class BenchMark {
             }
 
         };
-
-        proto.Employee empP = Employee.newBuilder().
-                setName("Kamal Sharma")
-                .setEmpId("PAYTM-26847")
-                .setAddress("Pawan Vihar Colony , Pithoragarh, Uttarakhand")
-                .setEmail("kamal.sharma@paytm.com")
-                .setPhoneNo(9911729459L).build();
 
         Runnable proto = () -> {
 
@@ -47,8 +64,22 @@ public class BenchMark {
 
         for (int i = 0; i < 100; i++) {
 
-            //runPerformanceTest(json, "JSON");
+            runPerformanceTest(json, "JSON");
+
+            try {
+                fileWriter.append("   |  ");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             runPerformanceTest(proto, "PROTO");
+
+            try {
+                fileWriter.append('\n');
+                fileWriter.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         }
 
@@ -56,7 +87,7 @@ public class BenchMark {
 
     private static void runPerformanceTest(Runnable runnable, String method){
         long time1 = System.currentTimeMillis();
-        for (int i = 0; i < 9000000; i++) {
+        for (int i = 0; i < 1000000; i++) {
             runnable.run();
         }
         long time2 = System.currentTimeMillis();
@@ -64,5 +95,18 @@ public class BenchMark {
         System.out.println(
                 method + " : " + (time2 - time1) + " ms"
         );
+
+        writeToFile(""+(time2-time1));
+    }
+
+    private static void writeToFile(String line){
+
+        try {
+
+            fileWriter.append(line);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
